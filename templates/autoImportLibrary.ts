a6b927acebc1fed9abe298bfeecc33e5d41e3cad
李茂峰 <limaofeng@msn.com>
2021-09-07 00:19:46 +0800

    备份代码

---
+++ templates/autoImportLibrary.ts
@@ -0,0 +1,6 @@
+// 加载默认组件库
+// LibraryManager.loadDefaultLibrarys();
+
+// {{#librarys}}
+// LibraryManager.addLibrary(require('{{{path}}}').default);
+// {{/librarys}}
---
+++ templates/runtime.tsx
@@ -0,0 +1,133 @@
+import { merge } from 'lodash';
+import React from 'react';
+import { createLogger } from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { client } from '../apollo';
+import ExtDvaContainer from './ExtDvaContainer';
+import {
+  getApplication as GET_APPLICATION,
+  subscibeUpdateRoute as SUBSCIBE_UPDATEROUTE,
+  getRoute as GET_ROUTE,
+} from './gql/application.gql';
+import { setCurrentApplication } from './models/global';
+// import { AppManager, EnvironmentManager } from '@asany/components';
+
+const logging = process.env.NODE_ENV === 'development';
+
+interface IApplication {
+  path: string;
+}
+
+interface IRoute {
+  path?: string;
+  name?: string;
+  type: 'menu' | 'header' | 'divider' | 'route';
+  component: any | React.SFC<any>;
+  configuration: any;
+  application: IApplication;
+  authorized: boolean;
+  authority: string[];
+  wrappers: any[];
+  routes: any[] | undefined;
+  exact: boolean;
+  parent: any;
+}
+
+const authRoutes = {};
+let extraRoutes: any[] = [];
+
+function ergodicRoutes(routes: any[], authKey: any, authority: any) {
+  routes.forEach(element => {
+    if (element.path === authKey) {
+      if (!element.authority) element.authority = []; // eslint-disable-line
+      Object.assign(element.authority, authority || []);
+    } else if (element.routes) {
+      ergodicRoutes(element.routes, authKey, authority);
+    }
+    return element;
+  });
+}
+
+async function loadRoutes() {
+  const {
+    data: { app },
+  } = await client.query({
+    query: GET_APPLICATION,
+    variables: {
+      id: '{{id}}',
+    },
+    fetchPolicy: 'no-cache',
+  });
+
+  setCurrentApplication(app);
+
+  // await import('./autoImportLibrary');
+
+  // const env = EnvironmentManager.currentEnvironment();
+
+  // env.set('layout.navbar.logo', app.logo);
+  // env.set('layout.navbar.title', app.name);
+
+  // extraRoutes = AppManager.transform(app.routes);
+}
+
+export const patchRoutes = ({ routes }: any) => {
+  routes.splice(1);
+  Object.keys(authRoutes).forEach(authKey =>
+    ergodicRoutes(routes, authKey, authRoutes[authKey].authority)
+  );
+  merge(routes, extraRoutes);
+};
+
+export const rootContainer = (container: any) => {
+  return React.createElement(ExtDvaContainer as any, null, container);
+};
+
+export const render = (oldRender: () => void) => {
+  loadRoutes().then(oldRender);
+};
+
+export const onRouteChange = () => {};
+
+const persistConfig = {
+  timeout: 1000, // you can define your time. But is required.
+  key: 'root',
+  storage,
+};
+
+const persistEnhancer = () => (createStore: any) => (
+  reducer: any,
+  initialState: any,
+  enhancer: any
+) => {
+  const store = createStore(
+    persistReducer(persistConfig, reducer),
+    initialState,
+    enhancer
+  );
+  const persistor = persistStore(store, null);
+  return {
+    persistor,
+    ...store,
+  };
+};
+
+export const dva = {
+  config: {
+    extraEnhancers: [persistEnhancer()],
+    onError(e: Error) {
+      console.error(e.message, e);
+    },
+  },
+  plugins: [
+    ...(logging
+      ? [
+          {
+            onAction: createLogger(),
+          },
+        ]
+      : []),
+  ],
+};
